d126688d163fcbca67fb9f4ac920942d7b91c532
CreamPuffx2 <exploit017@gmail.com>
2020-05-17 17:42:50 +0900

    test hashBlock() method

--- dev/test.js
+++ dev/test.js
@@ -2,15 +2,27 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(789457, 'OIUOEDJETH8754DHKD', '78SHNEG45DER56');
-bitcoin.createNewTransaction(100, 'ALEXHT845SJ5TKCJ2', 'JENN5BG5DF6HT8NG9');
+const previousBlockHash = 'A49F60628DE93E45DE06213D084A4B210E8CBB57126ACEE98718A04BB6B09A6E';
+const currentBlockData = [
+  {
+    amount: 10,
+    sender: '89BCF68A763D11F28341B3847B7C79F1DCA13D34F1F344184562F059D49DF216',
+    recipient: 'DEA42E88C1FC84536F62F5F6FE7B572CE09D479804DE3AFD7C4AB4C9B8BFF2F9',
+  },
+  {
+    amount: 90,
+    sender: 'DEA42E88C1FC84536F62F5F6FE7B572CE09D479804DE3AFD7C4AB4C9B8BFF2F9',
+    recipient: '89BCF68A763D11F28341B3847B7C79F1DCA13D34F1F344184562F059D49DF216',
+  },
+  {
+    amount: 20,
+    sender: '89BCF68A763D11F28341B3847B7C79F1DCA13D34F1F344184562F059D49DF216',
+    recipient: 'EA42E88C1FC84536F62F5F6FE7B572CE09D479804DE3AFD7C4AB4C9B8BFF2F9',
+  },
+];
+const nonce = 100;
 
-bitcoin.createNewBlock(548764, 'AKMC875E6S1RS9', 'WLS214R7T6SJ3G2');
-bitcoin.createNewTransaction(200, 'MIN01JAE43F1L2O34', 'H2KK93LL84JEONG43');
-bitcoin.createNewTransaction(900, 'H2KK93LL84JEONG43', 'MIN01JAE43F1L2O34');
-bitcoin.createNewTransaction(400, 'MIN01JAE43F1L2O34', 'H2KbircoinK93LL84JEONG43');
+let result = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
 
-bitcoin.createNewBlock(548764, 'AKMC875E6S1RS9', 'WLS214R7T6SJ3G2');
-
-console.log(bitcoin);
+console.log(result);
 // console.log(bitcoin.chain[bitcoin.chain.length - 1]);
