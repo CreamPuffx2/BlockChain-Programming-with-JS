e38c2624cc0d93142516d908e4a14260d4df8c06
CreamPuffx2 <exploit017@gmail.com>
2020-05-17 22:15:23 +0900

    setting api.js

--- dev/api.js
+++ dev/api.js
@@ -1,8 +1,18 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello world');
+app.get('/blockchain', function(req, res) {
+
 });
 
-app.listen(3000);
+app.post('/transaction', function(req, res) {
+
+});
+
+app.get('/mine', function(req, res) {
+
+});
+
+app.listen(3000 ,function() {
+  console.log('listening on port 3000...');
+});
