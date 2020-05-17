2ce25189c0c3bba8d268e31d82a9b81e8754cf2a
CreamPuffx2 <exploit017@gmail.com>
2020-05-17 18:00:58 +0900

    test proofOfWork() method

--- dev/test.js
+++ dev/test.js
@@ -1,28 +1,26 @@
 const Blockchain = require('./blockchain');
-
 const bitcoin = new Blockchain();
 
-const previousBlockHash = 'A49F60628DE93E45DE06213D084A4B210E8CBB57126ACEE98718A04BB6B09A6E';
+const previousBlockHash = 'OINAISDFN09N09ASDNF90N90ASNDF';
 const currentBlockData = [
   {
-    amount: 10,
-    sender: '89BCF68A763D11F28341B3847B7C79F1DCA13D34F1F344184562F059D49DF216',
-    recipient: 'DEA42E88C1FC84536F62F5F6FE7B572CE09D479804DE3AFD7C4AB4C9B8BFF2F9',
+    amount: 101,
+    sender: 'N90ANS90N90ANSDFN',
+    recipient: '90NA90SNDF90ANSDF09N'
   },
   {
-    amount: 90,
-    sender: 'DEA42E88C1FC84536F62F5F6FE7B572CE09D479804DE3AFD7C4AB4C9B8BFF2F9',
-    recipient: '89BCF68A763D11F28341B3847B7C79F1DCA13D34F1F344184562F059D49DF216',
+    amount: 30,
+    sender: '09ANS09DFNA8SDNF',
+    recipient: 'UIANSIUDFUIABSDUIFB'
   },
   {
-    amount: 20,
-    sender: '89BCF68A763D11F28341B3847B7C79F1DCA13D34F1F344184562F059D49DF216',
-    recipient: 'EA42E88C1FC84536F62F5F6FE7B572CE09D479804DE3AFD7C4AB4C9B8BFF2F9',
-  },
+    amount: 200,
+    sender: '89ANS89DFN98ASNDF89',
+    recipient: 'AUSDF89ANSD9FNASD'
+  }
 ];
-const nonce = 100;
 
-let result = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+let result = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
 
 console.log(result);
 // console.log(bitcoin.chain[bitcoin.chain.length - 1]);
