f136f5ae6e776df4a2021535222a80dc979c255a
mjdn0011 <exploit017@gmail.com>
2021-02-27 23:24:22 +0900

    /blockchain 엔드 포인트 구축

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const Blockchain = require('./blockchain');
+
 const app = express();
+const bitcoin = new Blockchain();
+
+// JSON 형태 요청 Parsing
 app.use(bodyParser.json());
+// Form-data 형태 요청 Parsing
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // 전체 블록체인을 가져와 데이터 확인
 app.get('/blockchain', function(req, res) {
-
+  res.send(bitcoin);
 });
 
 // 새로운 트랜잭션 생성
