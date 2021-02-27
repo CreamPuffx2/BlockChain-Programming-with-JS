a53b6167e09e6a445aaf32b004ae2349451160e5
mjdn0011 <exploit017@gmail.com>
2021-02-27 23:15:39 +0900

    /transaction end-point 구축

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // 전체 블록체인을 가져와 데이터 확인
 app.get('/blockchain', function(req, res) {
@@ -9,7 +12,9 @@ app.get('/blockchain', function(req, res) {
 
 // 새로운 트랜잭션 생성
 app.post('/transaction', function(req, res) {
+  console.log(req.body);
 
+  res.send(`The amount of the transaction is ${req.body.amount} bitcoin.`);
 });
 
 // PoW 메소드 사용 - 새 블록 채굴
